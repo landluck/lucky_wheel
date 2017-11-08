8e2ff017f8bc5cf86ed6fad316fd20812e590971
liuhaihong <liuhaihong@eastcom.com>
2017-11-08 13:54:16 +0800

    build config amend

--- build/utils.js
+++ build/utils.js
@@ -38,7 +38,8 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        publicPath: '../../'
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
