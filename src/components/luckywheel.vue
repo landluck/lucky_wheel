abb556fbce1a195f4529bce55b935b8ab1815740
刘海红 <liuhaihong@eastcom.com>
2018-02-25 17:30:32 +0800

    v1.1.0

--- src/components/luckywheel.vue
+++ src/components/luckywheel.vue
@@ -10,13 +10,13 @@
                     <div class="prize-list">
                         <div class="prize-item" v-for="(item,index) in prize_list" :key="index">
                             <div class="prize-pic">
-                                <img :src="item.picture">
+                                <img :src="item.icon">
                             </div>
-                            <div class="prize-count">
-                                {{item.change}}
+                            <div class="prize-count" v-if="item.count">
+                                {{item.count}}
                             </div>
                             <div class="prize-type">
-                                {{item.zh_name}}
+                                {{item.name}}
                             </div>
                         </div>
                     </div>
@@ -27,7 +27,6 @@
             <div class="main-bg"></div>
             <div class="bg-p"></div>
             <div class="content">
-                <div class="beans">您的金豆： {{ easejoy_bean}}豆</div>
                 <div class="lottery_ticket">今日免费抽奖次数： {{ lottery_ticket}}</div>
             </div>
             <div class="tip">
@@ -41,15 +40,13 @@
         </div>
         <div class="toast" v-show="toast_control">
             <div class="toast-container">
-                <img :src="toast_pictrue" class="toast-picture" v-show="toast_back">
-                <img src="../assets/img/wheel_toast_1.png" class="toast-pictrue-change" v-show="!toast_back">
-                <div class="close" v-show="!toast_back" @click="close_toast()"></div>
+                <img :src="toast_pictrue" class="toast-picture">
+                <div class="close" @click="close_toast()"></div>
                 <div class="toast-title">
                     {{toast_title}}
                 </div>
                 <div class="toast-btn">
-                    <div class="toast-confirm" v-if="!toast_back" @click="confirm_exchange()">确认</div>
-                    <div class="toast-cancel" v-else @click="close_toast">关闭</div>
+                    <div class="toast-cancel"  @click="close_toast">关闭</div>
                 </div>
             </div>
         </div>
@@ -57,17 +54,63 @@
     </div>
 </template>
 <script>
-import { prize_list_json } from "../assets/js/database.js" //此奖品数组为服务器请求得到的json数据,进过处理，赋值给prize_list
 export default {
   data() {
     return {
       easejoy_bean: 0, //金豆
       lottery_ticket: 0, //抽奖次数
-      prize_list: [], //奖品列表
+      prize_list: [
+        {
+          icon: require("../assets/img/bean_500.png"), // 奖品图片
+          count: 10, // 奖品数量
+          name: "易趣豆", // 奖品名称
+          isPrize: 1 // 该奖项是否为奖品
+        },
+        {
+          icon: require("../assets/img/bean_five.png"),
+          count: 5,
+          name: "豆",
+          isPrize: 1
+        },
+        {
+          icon: require("../assets/img/bean_one.png"),
+          count: 10,
+          name: "易趣豆",
+          isPrize: 1
+        },
+        {
+          icon: require("../assets/img/point_five.png"),
+          count: 5,
+          name: "积分",
+          isPrize: 1
+        },
+        {
+          icon: require("../assets/img/point_ten.png"),
+          count: 10,
+          name: "积分",
+          isPrize: 1
+        },
+        {
+          icon: require("../assets/img/bean_500.png"),
+          count: 10,
+          name: "易趣豆",
+          isPrize: 1
+        },
+        {
+          icon: require("../assets/img/give_up.png"),
+          count: 0,
+          name: "未中奖",
+          isPrize: 0
+        },
+        {
+          icon: require("../assets/img/bean_500.png"),
+          count: 10,
+          name: "易趣豆",
+          isPrize: 1
+        }
+      ], //奖品列表
       toast_control: false, //抽奖结果弹出框控制器
-      toast_pictrue: require("../assets/img/congratulation.png"), //抽奖结果标题图片src
-      toast_title: "恭喜你，获得10积分", //抽奖结果文本反馈
-      toast_back: false, //抽奖结果toast中是否为确认按钮
+      hasPrize: false, //是否中奖
       start_rotating_degree: 0, //初始旋转角度
       rotate_angle: 0, //将要旋转的角度
       start_rotating_degree_pointer: 0, //指针初始旋转角度
@@ -77,66 +120,34 @@ export default {
       click_flag: true, //是否可以旋转抽奖
       i: 0 //测试使用
     };
-  },  
-  created(){
-      this.init_prize_list(prize_list_json.prizes)
+  },
+  created() {
+    this.init_prize_list();
+  },
+  computed: {
+    toast_title() {
+      return this.hasPrize
+        ? "恭喜您，获得" +this.prize_list[this.i].count + ' ' + this.prize_list[this.i].name
+        : "未中奖";
+    },
+    toast_pictrue() {
+      return this.hasPrize
+        ? require("../assets/img/congratulation.png")
+        : require("../assets/img/sorry.png");
+    }
   },
   methods: {
-      //此方法为处理奖品数据
-    init_prize_list(list){
-        for(var i=0;i<list.length;i++){
-            var tmp = list[i];
-            if(tmp.name=='point'){
-                list[i].zh_name = '积分'
-            }else if(tmp.name == 'easejoy_bean'){
-                list[i].zh_name = '金豆'
-            }else{
-                list[i].zh_name = " "
-            }
-            list[i].picture = require("../assets/img/"+this.init_prize_icon(list[i].type));            
-        }
-        this.prize_list = list;
-        console.log(list);
-    },
-    //此方法为初始化奖品图标
-    init_prize_icon(type){
-        switch (type) {
-            case 'nothing':return 'give_up.png'
-                
-                break;
-            case '10-beans':return 'bean_ten.png'
-                
-                break;
-            case '5-point':return 'point_five.png'
-                
-                break;
-            case '5-beans':return 'bean_five.png'
-                
-                break;
-            case '10-point':return 'point_ten.png'
-                
-                break;
-            case '2-beans':return 'bean_two.png'
-                
-                break;
-            case 'most-beans':return 'bean_500.png'
-                
-                break;
-            case '1-bean':return 'bean_one.png'
-                
-                break;
-            
-        }
-    },
+    //此方法为处理奖品数据
+    init_prize_list(list) {},
     rotate_handle() {
       this.rotating();
-      this.i = this.i + 2;     
+      this.i = this.i + 2;
     },
     rotating(index) {
       if (!this.click_flag) return;
       var type = 0; // 默认为 0  转盘转动 1 箭头和转盘都转动(暂且遗留)
       var during_time = 5; // 默认为1s
-      var random = Math.floor(Math.random()*7)
+      var random = Math.floor(Math.random() * 7);
       var result_index = index || random; // 最终要旋转到哪一块，对应prize_list的下标
       var result_angle = [337.5, 292.5, 247.5, 202.5, 157.5, 112.5, 67.5, 22.5]; //最终会旋转到下标的位置所需要的度数
       var rand_circle = 6; // 附加多转几圈，2-3
@@ -157,44 +168,19 @@ export default {
         // 旋转结束后，允许再次触发
         setTimeout(function() {
           that.click_flag = true;
-           that.winning()
-        }, during_time * 1000);
+          that.game_over(this.i);
+        }, during_time * 1000 + 1500); // 延时，保证转盘转完
       } else {
         //
       }
     },
-    //中奖函数，需传入中奖结果 string 
-    winning(result){
-        this.toast_control = true;
-        this.toast_title = result?result:this.toast_title;
-        this.toast_back = true;
+    game_over() {
+      this.toast_control = true;
+      this.hasPrize = this.prize_list[this.i].isPrize
     },
-    //没有中奖函数，无需传入
-    fail(){
-        this.toast_control = true;
-        this.toast_pictrue =require('../assets/img/sorry.png');
-        this.toast_title = '不要灰心';
-        this.toast_back = true;
-    },
-    //我要金豆，-10换取抽奖机会,参数count number类型，使用count个金豆换取一次抽奖机会
-    exchange(count){
-       this.toast_control = true;     
-       this.toast_title = '-'+count+"金豆换取一次抽奖机会";
-       this.toast_back = false;
-    },
-    //金豆不足函数
-    quantity(){
-        this.toast_control = true;     
-       this.toast_title = "金豆数量不够";
-       this.toast_back = false;
-    },
-    //关闭抽奖结果
-    close_toast(){
-        this.toast_control = false;
-    },
-    //确认兑换函数
-    confirm_exchange(){
-        this.toast_control = false;
+    //关闭弹窗
+    close_toast() {
+      this.toast_control = false;
     }
   }
 };
@@ -413,12 +399,12 @@ export default {
   width: 18.75rem;
   height: 8.5625rem;
 }
-.toast-pictrue-change{
-    position: absolute;
-    top: -1.5rem;
-    left: -1.375rem;
-    width: 17.5rem;
-    height: 3.125rem;
+.toast-pictrue-change {
+  position: absolute;
+  top: -1.5rem;
+  left: -1.375rem;
+  width: 17.5rem;
+  height: 3.125rem;
 }
 .toast-title {
   padding: 2.8125rem 0;
@@ -426,11 +412,11 @@ export default {
   color: #fc7939;
   text-align: center;
 }
-.toast-btn{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: .9375rem;
+.toast-btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 0.9375rem;
 }
 .toast-btn div {
   background-image: -moz-linear-gradient(
@@ -460,14 +446,14 @@ export default {
   line-height: 1.875rem;
   color: #fff;
 }
-.close{
-    position: absolute;
-    top: -.9375rem;
-    right: -.9375rem;
-    width: 2rem;
-    height: 2rem;
-    background: url('../assets/img/close_store.png') no-repeat center top;
-    background-size: 100%;
+.close {
+  position: absolute;
+  top: -0.9375rem;
+  right: -0.9375rem;
+  width: 2rem;
+  height: 2rem;
+  background: url("../assets/img/close_store.png") no-repeat center top;
+  background-size: 100%;
 }
 </style>
 
